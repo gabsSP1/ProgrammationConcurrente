945553fe8accaac193e3217af5961152c9f8c355
gabsSP1 <gabs.dupont@gmail.com>
2016-03-16 17:06:24 +0100

    Update GestionClavier.h
    qsd

--- GestionClavier.h
+++ GestionClavier.h
@@ -1,83 +1,34 @@
 /*************************************************************************
-                           GestionClavier  -  description
+                           ${GestionClavier}  -  description
                              -------------------
     début                : ${date}
     copyright            : (C) ${year} par ${user}
 *************************************************************************/
 
-//---------- Réalisation de GestionClavier (fichier GestionClavier.cpp) --
-
-//---------------------------------------------------------------- INCLUDE
-
-//-------------------------------------------------------- Include système
-using namespace std;
-#include <iostream>
+//---------- Interface de la classe <GestionClavier> (fichier ${file_name}) ------
+#if ! defined ( GESTIONCLAVIER_H )
+#define GESTIONCLAVIER_H
+//--------------------------------------------------- Interfaces utilisées
 #include <stdlib.h>
 #include <unistd.h>
 
-//------------------------------------------------------ Include personnel
-#include "GestionClavier.h"
-#include "Menu.h"
 
-//------------------------------------------------------------- Constantes
-
-//---------------------------------------------------- Variables de classe
-
-//----------------------------------------------------------- Types privés
-
-
-//----------------------------------------------------------------- PUBLIC
-//-------------------------------------------------------- Fonctions amies
-
-//----------------------------------------------------- Méthodes publiques
-pid_t processusMenu;
-
-void GestionClavier::GestionClavier()
-// Algorithme :
-//
-{
-	
-	
-	if( ( proceMenu=fork() ) == 0 )
-	{
-		Menu();
-	}
-	
-	else 
-	{
-		for(;;)
-		{
-			pause();
-		}
-	}
-	
-	
-} //----- Fin de Méthode
-
-void GestionClavier::Commande(char code, unsigned int valeur)
-// Algorithme :
-//
-{
-	switch(code)
-	{
-		case(e):
-			terminerTache();
-		break;
-	}
-	
-} //----- Fin de Méthode
+    void GestionClavier();
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    
+     void Commande(char code, unsigned int valeur);
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+ 
 
 
-void terminerTache()
-{
-	kill(processusMenu, SIGUSR2);
-	exit(0);
-}
+//------------------------------------------------------- Attributs privés
 
 
+#endif // GESTIONCLAVIER_H
 
-//------------------------------------------------------------------ PRIVE
-
-//----------------------------------------------------- Méthodes protégées
-
-//------------------------------------------------------- Méthodes privées
